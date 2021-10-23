5e05933964789428c6c9b9e2dda229e007eeb6f5
Zixian Cai <2891235+caizixian@users.noreply.github.com>
2021-10-23 22:04:47 +1100

    Basic models

--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,19 @@
 use crate::routes::*;
+use actix_files::Files;
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
+use handlebars::Handlebars;
 use sqlx::PgPool;
 use std::net::TcpListener;
-use actix_files::Files;
-use handlebars::Handlebars;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
     let mut handlebars = Handlebars::new();
     handlebars.set_strict_mode(true);
-    handlebars.register_templates_directory(".html", "./static/templates").unwrap();
+    handlebars
+        .register_templates_directory(".html", "./static/templates")
+        .unwrap();
     let handlebars_ref = web::Data::new(handlebars);
     let server = HttpServer::new(move || {
         App::new()
