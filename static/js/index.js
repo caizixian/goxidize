6fdbae6fbfab36af1f2ffcdb199b6e01d8369dc2
Zixian Cai <2891235+caizixian@users.noreply.github.com>
2021-10-24 15:32:25 +1100

    Use React for frontend

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const element = <h1>Hello, world</h1>;
+ReactDOM.render(element, document.getElementById('app'));
--- webpack.common.js
+++ webpack.common.js
@@ -9,6 +9,16 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }
+            },
+            {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
