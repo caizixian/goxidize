53c14b778bcc3dafa53ace09c318f3b69005330e
Zixian Cai <2891235+caizixian@users.noreply.github.com>
2021-10-25 01:10:37 +1100

    Add form for adding links

---
+++ src/frontend/index.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class LinkForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {path: '', destination: 'https://'};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+
+        this.setState({
+           [target.name]: target.value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        if (this.state.destination.startsWith("http") && this.state.path !== "") {
+            const options = {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(this.state)
+            };
+            fetch('/api/v1/link', options)
+                .then(_ => this.setState({path: '', destination: 'https://'}));
+        }
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} className="col-4">
+                <div className="form-group">
+                    <label htmlFor="path">Path</label>
+                    <input type="text" className="form-control" id="path" name="path" value={this.state.path} placeholder="Enter path" onChange={this.handleChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="destination">Destination</label>
+                    <input type="text" className="form-control" id="destination" name="destination" value={this.state.destination} placeholder="Enter destination" onChange={this.handleChange} />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        );
+    }
+}
+
+ReactDOM.render(<LinkForm />, document.getElementById('app'));
--- src/frontend/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const element = <h1>Hello, world!</h1>;
-ReactDOM.render(element, document.getElementById('app'));
