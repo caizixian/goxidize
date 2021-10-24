b26ec5ada45da5c302724c6fb665bc35dd57e043
Zixian Cai <2891235+caizixian@users.noreply.github.com>
2021-10-25 01:06:04 +1100

    Add API endpoint for adding links

--- src/routes/api_v1.rs
+++ src/routes/api_v1.rs
@@ -1,4 +1,19 @@
-use actix_web::web;
+use crate::models::{Link, LinkFormData};
+use actix_web::{post, web, HttpResponse, Responder};
+use sqlx::PgPool;
 
-pub fn config(_cfg: &mut web::ServiceConfig) {
+#[post("/link")]
+async fn new_link(link: web::Json<LinkFormData>, pg: web::Data<PgPool>) -> impl Responder {
+    let row = Link::from_form_data(link.into_inner())
+        .upsert(pg.get_ref())
+        .await;
+    if let Err(e) = row {
+        println!("{:?}", e);
+        return HttpResponse::InternalServerError().finish();
+    }
+    HttpResponse::Ok().finish()
+}
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(new_link);
 }
