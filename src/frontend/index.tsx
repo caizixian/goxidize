82ba52d5b5472a2a78af64d3b041bcc6cd30590c
Zixian Cai <2891235+caizixian@users.noreply.github.com>
2021-10-25 20:19:32 +1100

    Use TypeScript

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,26 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class LinkForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.initialState = {path: '', destination: 'https://'};
-        this.state = this.initialState;
+type LinkFormProps = {};
+type LinkFormState = {
+    path: string,
+    destination: string
+}
 
+class LinkForm extends React.Component<LinkFormProps, LinkFormState> {
+    private initialState = {path: '', destination: 'https://'};
+    state: LinkFormState;
+
+    constructor(props: LinkFormProps) {
+        super(props);
+        this.state = this.initialState;
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        const target = event.target;
-
+    private readonly handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+        const {name, value} = event.currentTarget;
         this.setState({
-           [target.name]: target.value
-        });
-    }
+            [name]: value
+        } as Pick<LinkFormState, keyof LinkFormState>);
+    };
 
-    handleSubmit(event) {
+    private readonly handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         if (this.state.destination.startsWith("http") && this.state.path !== "") {
             const options = {
@@ -38,11 +44,13 @@ class LinkForm extends React.Component {
             <form onSubmit={this.handleSubmit} className="col-4">
                 <div className="form-group">
                     <label htmlFor="path">Path</label>
-                    <input type="text" className="form-control" id="path" name="path" value={this.state.path} placeholder="Enter path" onChange={this.handleChange} />
+                    <input type="text" className="form-control" id="path" name="path" value={this.state.path}
+                           placeholder="Enter path" onChange={this.handleChange}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="destination">Destination</label>
-                    <input type="text" className="form-control" id="destination" name="destination" value={this.state.destination} placeholder="Enter destination" onChange={this.handleChange} />
+                    <input type="text" className="form-control" id="destination" name="destination"
+                           value={this.state.destination} placeholder="Enter destination" onChange={this.handleChange}/>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
@@ -50,4 +58,4 @@ class LinkForm extends React.Component {
     }
 }
 
-ReactDOM.render(<LinkForm />, document.getElementById('app'));
+ReactDOM.render(<LinkForm/>, document.getElementById('app'));
