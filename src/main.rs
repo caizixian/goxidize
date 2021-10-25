73945e8a887b40298d3d78e1cf2fd5333d802348
Zixian Cai <2891235+caizixian@users.noreply.github.com>
2021-10-25 13:57:26 +1100

    Change the default logging level for debug env

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("warn")).init();
     let configuration = get_configuration().expect("Failed to read configuration.yml.");
+    let default_logging_level = if configuration.debug { "info" } else { "warn" };
+    env_logger::Builder::from_env(Env::default().default_filter_or(default_logging_level)).init();
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
